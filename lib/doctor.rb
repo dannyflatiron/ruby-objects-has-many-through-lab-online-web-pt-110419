fb5d3185a6c46d4465aa61c349743603bb48e8d6
Daniel Reina <dannyflatiron@gmail.com>
2019-12-22 18:22:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,14 +1,17 @@
 class Doctor
 
-attr_accessor
-attr_reader :name
+attr_accessor :name
+attr_reader 
 
 @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
+  end
 
+  def self.all
+    @@all
   end
 
 
